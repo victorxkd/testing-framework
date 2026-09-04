96b604ab1483c1826f5859895e050fd78e02a4dc
victorxkd <oliveira.victorfc@gmail.com>
2026-09-04 20:49:36 -0300

    7. Comandos Assert

--- tests/test_test_case.py
+++ tests/test_test_case.py
@@ -96,6 +96,37 @@ class TestCaseTest(TestCase):
 
         assert spy.log == "set_up test_method tear_down"
 
+    def test_assert_true(self):
+        self.assert_true(True)
+
+    def test_assert_false(self):
+        self.assert_false(False)
+
+    def test_assert_equal(self):
+        self.assert_equal("", "")
+        self.assert_equal("foo", "foo")
+
+        self.assert_equal([], [])
+        self.assert_equal(["foo"], ["foo"])
+
+        self.assert_equal((), ())
+        self.assert_equal(("foo",), ("foo",))
+
+        self.assert_equal({}, {})
+        self.assert_equal({"foo"}, {"foo"})
+
+    def test_assert_in(self):
+        animals = {
+            "monkey": "banana",
+            "cow": "grass",
+            "seal": "fish"
+        }
+
+        self.assert_in("a", "abc")
+        self.assert_in("foo", ["foo"])
+        self.assert_in(1, [1, 2, 3])
+        self.assert_in("monkey", animals)
+
 
 if __name__ == "__main__":
     result = TestResult()
@@ -122,6 +153,18 @@ if __name__ == "__main__":
     test.run(result)
 
     test = TestCaseTest("test_template_method")
+    test.run(result)
+
+    test = TestCaseTest("test_assert_true")
+    test.run(result)
+
+    test = TestCaseTest("test_assert_false")
+    test.run(result)
+
+    test = TestCaseTest("test_assert_equal")
+    test.run(result)
+
+    test = TestCaseTest("test_assert_in")
     test.run(result)
 
     print(result.summary())
